d0e1098e0a8f7860c43a484504295b5d57764bbc
BlackBook <jamieb@ziggo.nl>
2021-03-22 06:24:51 +0100

    Update beard.lua

--- client/beard.lua
+++ client/beard.lua
@@ -1,7 +1,7 @@
 -- Grow process
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(Config.minutes * 60000)
+        Citizen.Wait(Config.beardminutes * 60000)
         TriggerEvent('skinchanger:getSkin', function(skinData)
             if skinData ~= nil then
                 if skinData['beard_2'] > 1 and skinData['beard_2'] < 10 then
