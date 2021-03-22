d6a7e3918a461f60d0c98035a7c7f1562f2904e6
BlackBook <jamieb@ziggo.nl>
2021-03-22 06:48:54 +0100

    Update eyebrow.lua

--- client/eyebrow.lua
+++ client/eyebrow.lua
@@ -53,16 +53,3 @@ if Config.EnableNotify then
         end
     end)
 end
-
-RegisterNetEvent('bb_beardshave:trim')
-AddEventHandler('bb_beardshave:trim', function()
-    TriggerEvent('skinchanger:getSkin', function(skinData)
-        if skinData ~= nil then
-            if skinData['eyebrows_2'] > 1 then
-                skinData['eyebrows_2'] = 1
-                TriggerEvent('skinchanger:loadSkin', skinData)
-                TriggerServerEvent('esx_skin:save', skinData)
-            end
-        end
-    end)
-end)
