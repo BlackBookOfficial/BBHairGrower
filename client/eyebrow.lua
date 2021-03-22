2801951934cc67948347e4a7c3786541d11b8513
BlackBook <jamieb@ziggo.nl>
2021-03-22 06:24:40 +0100

    Update eyebrow.lua

--- client/eyebrow.lua
+++ client/eyebrow.lua
@@ -1,7 +1,7 @@
 -- Grow process
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(Config.minutes * 60000)
+        Citizen.Wait(Config.browminutes * 60000)
         TriggerEvent('skinchanger:getSkin', function(skinData)
             if skinData ~= nil then
                 if skinData['eyebrows_2'] > 1 and skinData['eyebrows_2'] < 10 then
