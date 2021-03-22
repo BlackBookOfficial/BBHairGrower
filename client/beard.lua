7aca8543f8c397763cd549f35945c5ce48b86d70
BlackBook <jamieb@ziggo.nl>
2021-03-22 06:49:13 +0100

    Update beard.lua

--- client/beard.lua
+++ client/beard.lua
@@ -53,16 +53,3 @@ if Config.EnableNotify then
         end
     end)
 end
-
-RegisterNetEvent('bb_beardshave:shave')
-AddEventHandler('bb_beardshave:shave', function()
-    TriggerEvent('skinchanger:getSkin', function(skinData)
-        if skinData ~= nil then
-            if skinData['beard_2'] > 1 then
-                skinData['beard_2'] = 1
-                TriggerEvent('skinchanger:loadSkin', skinData)
-                TriggerServerEvent('esx_skin:save', skinData)
-            end
-        end
-    end)
-end)
