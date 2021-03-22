b0c8d3936ac2d9a1548fbcfc9a732cfdb5cb221b
BlackBook <jamieb@ziggo.nl>
2021-03-22 06:27:01 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.bearminutes = 15 -- How long it takes for beard to grow.
+Config.browminutes = 30 -- How long it takes for brows to grow.
+Config.EnableNotify = false -- Enabling of notifications.
+Config.notify = 10 -- Show notification over time.
